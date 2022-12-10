ed97b79f39ca6976b16d5ee36ad6cfca6164da7c
Markus Wagner <markus.wagner02@web.de>
2022-12-10 08:41:38 +0100

    add day 10

---
+++ Day10a/Program.cs
@@ -0,0 +1,46 @@
+﻿var cycle = 0;
+var x = 1;
+var relevantCycles = new[] { 20, 60, 100, 140, 180, 220 };
+
+var sum = 0;
+foreach (string line in File.ReadLines(@"../../../Input.txt"))
+{
+	var lineParts = line.Split(' ');
+	int oldCycle = cycle;
+	var oldX = x;
+	if (lineParts[0] == "noop")
+	{
+		cycle++;
+		sum = CheckForRelevantCycle(sum, cycle, x);
+	}
+	else if (lineParts[0] == "addx")
+	{
+		cycle++;
+		sum = CheckForRelevantCycle(sum, cycle, x);
+		cycle++;
+		sum = CheckForRelevantCycle(sum, cycle, x);
+		x += int.Parse(lineParts[1]);
+	}
+	else
+	{
+		throw new Exception($"illegal command {line}");
+	}
+
+	Console.WriteLine($"{line}: from {oldCycle}/{oldX} to {cycle}/{x}");
+}
+
+int CheckForRelevantCycle(int sum, int cycle, int x)
+{
+	if (relevantCycles.Contains(cycle))
+	{
+		Console.WriteLine($"{cycle}/{x} ==> {(cycle) * x}");
+		sum += cycle * x;
+	}
+	else
+	{
+		Console.WriteLine($"{cycle}/{x}");
+	}
+	return sum;
+}
+
+Console.WriteLine(sum);
---
+++ Day10b/Program.cs
@@ -0,0 +1,40 @@
+﻿var cycle = 0;
+var x = 1;
+
+var drawing = "";
+foreach (string line in File.ReadLines(@"../../../Input.txt"))
+{
+	var lineParts = line.Split(' ');
+	int oldCycle = cycle;
+	var oldX = x;
+	if (lineParts[0] == "noop")
+	{
+		cycle++;
+		drawing = DrawSprite(drawing, cycle, x);
+	}
+	else if (lineParts[0] == "addx")
+	{
+		cycle++;
+		drawing = DrawSprite(drawing, cycle, x);
+		cycle++;
+		drawing = DrawSprite(drawing, cycle, x);
+		x += int.Parse(lineParts[1]);
+	}
+	else
+	{
+		throw new Exception($"illegal command {line}");
+	}
+}
+
+string DrawSprite(string drawing, int cycle, int x)
+{
+	var column = (cycle - 1) % 40;
+	drawing += Math.Abs(column - x) <= 1 ? "#" : ".";
+	if (column == 39)
+	{
+		drawing += "\n";
+	}
+	return drawing;
+}
+
+Console.WriteLine(drawing);
