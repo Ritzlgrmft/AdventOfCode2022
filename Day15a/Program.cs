11cb47781cdb700028aefaf042c565e70f935b8e
Markus Wagner <markus.wagner02@web.de>
2022-12-15 08:47:25 +0100

    add day 15

---
+++ Day15a/Program.cs
@@ -0,0 +1,56 @@
+﻿const int rowOfInterest = 2000000;
+//const int rowOfInterest = 10;
+var notPossiblePositionRanges = new List<(int, int)>();
+var knownBeaconsInRowOfInterest = new List<int>();
+
+foreach (var line in File.ReadLines(@"../../../Input.txt"))
+{
+	Console.WriteLine(line);
+
+	var lineParts = line.Split(new string[] { "Sensor at x=", ", y=", ": closest beacon is at x=" }, StringSplitOptions.RemoveEmptyEntries);
+	var sensor = (int.Parse(lineParts[0]), int.Parse(lineParts[1]));
+	var beacon = (int.Parse(lineParts[2]), int.Parse(lineParts[3]));
+	if (beacon.Item2 == rowOfInterest && !knownBeaconsInRowOfInterest.Contains(beacon.Item1))
+	{
+		knownBeaconsInRowOfInterest.Add(beacon.Item1);
+	}
+
+	var distance = Math.Abs(sensor.Item1 - beacon.Item1) + Math.Abs(sensor.Item2 - beacon.Item2);
+	var distanceToRowOfInterest = Math.Abs(sensor.Item2 - rowOfInterest);
+	var distanceDelta = distance - distanceToRowOfInterest;
+
+	if (distanceDelta >= 0)
+	{
+		notPossiblePositionRanges.Add((sensor.Item1 - distanceDelta, sensor.Item1 + distanceDelta));
+		Console.WriteLine($"Add range from {sensor.Item1 - distanceDelta} to {sensor.Item1 + distanceDelta}");
+	}
+
+}
+
+var index = 0;
+while (index < notPossiblePositionRanges.Count - 1)
+{
+	var rangeToCheck = notPossiblePositionRanges[index];
+	var hasJoined = false;
+	for (var otherIndex = index + 1; otherIndex < notPossiblePositionRanges.Count && !hasJoined; otherIndex++)
+	{
+		var otherRangeToCheck = notPossiblePositionRanges[otherIndex];
+		if (rangeToCheck.Item1 <= otherRangeToCheck.Item1 && rangeToCheck.Item2 >= otherRangeToCheck.Item1 ||
+			rangeToCheck.Item1 >= otherRangeToCheck.Item1 && rangeToCheck.Item1 <= otherRangeToCheck.Item2)
+		{
+			rangeToCheck.Item1 = Math.Min(rangeToCheck.Item1, otherRangeToCheck.Item1);
+			rangeToCheck.Item2 = Math.Max(rangeToCheck.Item2, otherRangeToCheck.Item2);
+			notPossiblePositionRanges[index] = rangeToCheck;
+			notPossiblePositionRanges.RemoveAt(otherIndex);
+			hasJoined = true;
+		}
+	}
+	if (!hasJoined)
+	{
+		index++;
+	}
+}
+
+var count = notPossiblePositionRanges.Select(r => r.Item2 - r.Item1 + 1).Sum();
+
+Console.WriteLine(count - knownBeaconsInRowOfInterest.Count);
---
+++ Day15b/Program.cs
@@ -0,0 +1,78 @@
+﻿const int min = 0;
+const int max = 4000000;
+
+int y = min;
+var x = min - 1;
+while (y <= max && x < min)
+{
+	if (y % 10000 == 0)
+	{
+		Console.WriteLine(y);
+	}
+	var notPossiblePositionRanges = GetNotPossiblePositionRanges(y).Where(r => r.Item2 >= min && r.Item1 <= max);
+	if (notPossiblePositionRanges.Count() > 1)
+	{
+		x = notPossiblePositionRanges.First().Item2 + 1;
+	}
+	else if (notPossiblePositionRanges.First().Item1 > min)
+	{
+		x = notPossiblePositionRanges.First().Item1 - 1;
+	}
+	else if (notPossiblePositionRanges.Last().Item2 < max)
+	{
+		x = notPossiblePositionRanges.Last().Item2 + 1;
+	}
+	else
+	{
+		y++;
+	}
+}
+var frequency = ((long)x) * 4000000L + y;
+Console.WriteLine($"x={x}, y={y}, frequency={frequency}");
+
+
+
+List<(int, int)> GetNotPossiblePositionRanges(int rowOfInterest)
+{
+	var notPossiblePositionRanges = new List<(int, int)>();
+	foreach (var line in File.ReadLines(@"../../../Input.txt"))
+	{
+		var lineParts = line.Split(new string[] { "Sensor at x=", ", y=", ": closest beacon is at x=" }, StringSplitOptions.RemoveEmptyEntries);
+		var sensor = (int.Parse(lineParts[0]), int.Parse(lineParts[1]));
+		var beacon = (int.Parse(lineParts[2]), int.Parse(lineParts[3]));
+
+		var distance = Math.Abs(sensor.Item1 - beacon.Item1) + Math.Abs(sensor.Item2 - beacon.Item2);
+		var distanceToRowOfInterest = Math.Abs(sensor.Item2 - rowOfInterest);
+		var distanceDelta = distance - distanceToRowOfInterest;
+
+		if (distanceDelta >= 0)
+		{
+			notPossiblePositionRanges.Add((sensor.Item1 - distanceDelta, sensor.Item1 + distanceDelta));
+		}
+	}
+
+	var index = 0;
+	while (index < notPossiblePositionRanges.Count - 1)
+	{
+		var rangeToCheck = notPossiblePositionRanges[index];
+		var hasJoined = false;
+		for (var otherIndex = index + 1; otherIndex < notPossiblePositionRanges.Count && !hasJoined; otherIndex++)
+		{
+			var otherRangeToCheck = notPossiblePositionRanges[otherIndex];
+			if (rangeToCheck.Item1 <= otherRangeToCheck.Item1 && rangeToCheck.Item2 >= otherRangeToCheck.Item1 ||
+				rangeToCheck.Item1 >= otherRangeToCheck.Item1 && rangeToCheck.Item1 <= otherRangeToCheck.Item2)
+			{
+				rangeToCheck.Item1 = Math.Min(rangeToCheck.Item1, otherRangeToCheck.Item1);
+				rangeToCheck.Item2 = Math.Max(rangeToCheck.Item2, otherRangeToCheck.Item2);
+				notPossiblePositionRanges[index] = rangeToCheck;
+				notPossiblePositionRanges.RemoveAt(otherIndex);
+				hasJoined = true;
+			}
+		}
+		if (!hasJoined)
+		{
+			index++;
+		}
+	}
+	return notPossiblePositionRanges;
+}
